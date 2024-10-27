44c19a9cc1054c18ec7db44b598a5a206b672357
fernandochicote <fechicot@gmail.com>
2024-10-27 20:00:41 +0100

    Changes to be committed: 	modified:   api/main.py 	modified:   api/requirements.txt 	modified:   docker-compose.yml

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exception_handlers import http_exception_handler
 from fastapi.responses import RedirectResponse
 import uvicorn
 from  routers import holamundo, movies, kv
 from logger import log
+from elasticapm.contrib.starlette import ElasticAPM, make_apm_client
 
 descripcion = 'Aplicación sencilla para mostrar funcionalidades de FastAPI'
 
@@ -41,6 +43,25 @@ app.include_router(kv.router)
 def redirigir():
     log.info("Petición a /, redirigiendo a /docs.")
     return RedirectResponse(url="/docs")
+
+# APM
+
+# APM
+apm = make_apm_client({
+    'SERVICE_NAME': 'fastapi-api',
+    'DEBUG': True,
+    'SERVER_URL': 'http://apm-server:8200',
+    'CAPTURE_HEADERS': True,
+    'CAPTURE_BODY': 'all'
+})
+
+app.add_middleware(ElasticAPM, client=apm)
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    log.info(f"Capturando HTTPException motivo {exc.detail}")
+    apm.capture_exception()
+    return await http_exception_handler(request, exc)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
